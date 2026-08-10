6ec9fd8afe29ab8f257fb1310adfa4ebc0e387d2
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2026-08-10 11:31:04 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+main(){
+  List<String> noms = [ "Chadrac", "Ulric"];
+  for(var nom in noms){
+    print(nom);
+  }
+  int age;
+  String nom;
+  bool retour;
+  double moy;
+  
+  print("salut Chad");
+  print("salut Chads");
+}
