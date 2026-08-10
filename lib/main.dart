e0a7643d2cf2fd3c48630678a4f15497ac929d62
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2026-08-10 11:51:52 +0200

    main.dart updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,3 @@
 main(){
-  List<String> noms = [ "Chadrac", "Ulric"];
-  for(var nom in noms){
-    print(nom);
-  }
-  int age;
-  String nom;
-  bool retour;
-  double moy;
   
-  print("salut Chad");
-  print("salut Chads");
 }
