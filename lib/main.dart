ade57ce17f477c752f11032409d72c887bb5d4c5
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2026-08-11 09:35:37 +0200

    feat simple n alternaive structures

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,23 @@
 main(){
-  
+  int age = 18;
+
+  //STRUCTURES CONDITIONNELLLES
+
+  // SIMPLE
+  if(age < 18){
+    print("Mineur");
+  }else{
+    print("Majeur");
+  }
+
+  // ALTERNATIVE
+  if(age < 18){
+    print("Mineur");
+  }else if(age == 18){
+    print("Age Complique");
+  }else{
+    print("Majeur");
+  }
+  // IMBRIQUEE
+  // TERCIAIRE
 }
