9a4ef87b8b9f1701ce23140faed68d59047d1f0c
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2026-08-13 09:17:05 +0200

    feat Lst checked

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,82 @@
 main(){
-  int age = 18;
-  String sexe = "Masculin";
+  // int age = 18;
+  // String sexe = "Masculin";
 
-  //STRUCTURES CONDITIONNELLLES
+  // //STRUCTURES CONDITIONNELLLES
 
-  // SIMPLE
-  if(age < 18){
-    print("Mineur");
-  }else{
-    print("Majeur");
-  }
+  // // SIMPLE
+  // if(age < 18){
+  //   print("Mineur");
+  // }else{
+  //   print("Majeur");
+  // }
 
-  // ALTERNATIVE
-  if(age < 18){
-    print("Mineur");
-  }else if(age == 18){
-    print("Age Compliqué");
-  }else{
-    print("Majeur");
-  }
+  // // ALTERNATIVE
+  // if(age < 18){
+  //   print("Mineur");
+  // }else if(age == 18){
+  //   print("Age Compliqué");
+  // }else{
+  //   print("Majeur");
+  // }
 
-  // IMBRIQUEE
-  if(age < 18){
-    if(sexe == "Masculin"){
-      print("Mineur");
-    }else if(sexe == "Féminin"){
-      print("Mineure");
-    }
-  }else if(age == 18){
-    if(sexe == "Masculin"){
-      print("Age compliqué M");
-    }else if(sexe == "Féminin"){
-      print("Age compliqué F");
-    }
-  }else{
-    if(sexe == "Masculin"){
-      print("Majeur");
-    }else if(sexe == "Féminin"){
-      print("Majeure");
-    }
-  }
+  // // IMBRIQUEE
+  // if(age < 18){
+  //   if(sexe == "Masculin"){
+  //     print("Mineur");
+  //   }else if(sexe == "Féminin"){
+  //     print("Mineure");
+  //   }
+  // }else if(age == 18){
+  //   if(sexe == "Masculin"){
+  //     print("Age compliqué M");
+  //   }else if(sexe == "Féminin"){
+  //     print("Age compliqué F");
+  //   }
+  // }else{
+  //   if(sexe == "Masculin"){
+  //     print("Majeur");
+  //   }else if(sexe == "Féminin"){
+  //     print("Majeure");
+  //   }
+  // }
 
-  // TERNAIRE
-  print ((age < 18) ? 
-  ((sexe=="Masculin") ? "Homme Mineur" : "Femme mineure" ) : 
-  ((sexe=="Masculin") ? "Homme Majeur" : "Femme Majeure" )); 
+  // // TERNAIRE
+  // print ((age < 18) ? 
+  // ((sexe=="Masculin") ? "Homme Mineur" : "Femme mineure" ) : 
+  // ((sexe=="Masculin") ? "Homme Majeur" : "Femme Majeure" )); 
+
+
+
+// COLLECTIONS
+//LIST
+// List <int> ages = [13,20, 24, 10, 2];
+// List <String> noms = [" Ulric","Chadrac","Lucia"];
+// bool x = false;
+
+// print (ages[3]);
+// ages.add(56);
+
+// print (ages[5]);
+// print (ages.length);
+
+// ages.removeAt(5);
+// print(ages.length);
+
+// ages.remove(10);
+// print (ages[3]);
+
+// print(noms.contains("chadrac"));
+
+// for(var nom in noms){
+//   if(nom.toUpperCase() == "chadrac".toUpperCase()){
+//     x = true;
+//   }
+// }
+
+// print(x);
+
+// MAP
 }
 
 
